19960ca8fbcaa7e8443ab3009122bcc68cea2ed7
Nasser <nasseremil25@gmail.com>
2022-05-30 10:12:58 -0400

    Agregue font awesome y google font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMockupComponent } from './main-mockup/main-mockup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMockupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-mockup/main-mockup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-mockup',
+  templateUrl: './main-mockup.component.html',
+  styleUrls: ['./main-mockup.component.scss']
+})
+export class MainMockupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
