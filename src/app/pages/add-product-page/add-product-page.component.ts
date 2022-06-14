71329a55d3d7252be54378cd1615d289079da419
Nasser <nasseremil25@gmail.com>
2022-06-13 22:08:32 -0400

    add product y product page estan creados

--- src/app/pages/add-product-page/add-product-page.component.ts
+++ src/app/pages/add-product-page/add-product-page.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductPageComponent implements OnInit {
 
+  nombreORazonSocial: string = '';
+  tipoDeDocumento: string = '';
+  idDelProducto: number = 0;
+  precioDeVenta: number = 0;
+  precioDeCompra: number = 0;
+  Ganancia: number = 0;
+  DescripcionDelProducto: string = '';
+  nombreDelProveedor: string = '';
+  numeroDeDocumento: number = 0;
+  correoClientes: string = '';
+  telefono: number = 0;
+  celular: number = 0;
+  direccion: string = '';
+  comentarios: string = '';
   constructor() { }
 
   ngOnInit(): void {
