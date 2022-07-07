bf6eab58db1ba12b9d4b27a37340cf87a83b0c9a
Nasser <nasseremil25@gmail.com>
2022-07-07 16:06:18 -0400

    modal de clientes mejorado

--- src/app/pages/client-page/client-page.component.ts
+++ src/app/pages/client-page/client-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { style } from '@angular/animations';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-client-page',
@@ -7,12 +8,33 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class ClientPageComponent implements OnInit {
   ruta: string = '../client-page/add-client-page';
-  constructor() { }
+  constructor(private renderer2: Renderer2) { }
+  @ViewChild("checkbox") checkbox!: ElementRef;
+  @ViewChild("modal") modal: ElementRef;
+  @ViewChild("AsTipoComprobante") tipoComprobante: ElementRef;
+  @ViewChild("AsTipoDocumento") tipoDocumento: ElementRef;
+  @ViewChild("AsInputNumDoc") inputNumDoc: ElementRef;
 
-  @ViewChild("modal") modal!: ElementRef;
+  obtenerValue(): void{
+    const AsTipoComprobante = this.tipoComprobante.nativeElement;
+    const AsTipoDocumento = this.tipoDocumento.nativeElement;
+    const AsInputNumDoc = this.inputNumDoc.nativeElement;
+    
+    if(AsTipoComprobante.value === "Factura de Crédito Fiscal (Tipo 01)"){
+      AsTipoDocumento.innerHTML = `<option class="text-sm" selected disabled value="RNC">RNC</option>`
+      AsInputNumDoc.placeholder = "B0100000005"
+
+
+    }else if(AsTipoComprobante.value === "Factura de Consumo (Tipo 02)"){
+      AsTipoDocumento.innerHTML = `<option class="text-sm" selected disabled value="Cedula">Cedula</option>`
+      AsInputNumDoc.placeholder = "40247755893"
+    }
+    // this.renderer2.setStyle(AsTipoComprobante, style:'color', value:'red');
+  }
   ngOnInit(): void {
     this.openModal = this.openModal.bind(this)
   }
+  
 
   openModal(){
     this.modal.nativeElement.showModal()
